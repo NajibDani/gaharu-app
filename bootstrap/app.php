b6595244acb71ddf84c26a243898a697a0381c1a
NajibDani <najibdani21@gmail.com>
2026-09-04 08:36:14 +0700

    Fix insecure ketika login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Percayai semua proxy di depan Laravel (Traefik / Coolify)
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
         ]);
